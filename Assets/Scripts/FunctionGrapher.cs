21988f65e15ac89e26a8476b020fda74d5cfac4f
rotolonico <rotolonico@gmail.com>
2020-02-23 11:35:39 +0100

    Kept working on graphing

--- Assets/Scripts/FunctionGrapher.cs
+++ Assets/Scripts/FunctionGrapher.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using ChartAndGraph;
 using UnityEngine;
 
 public class FunctionGrapher : MonoBehaviour
 {
     public static FunctionGrapher Instance;
+
+    public float thickness = 0.1f;
 
     [SerializeField] private Transform graphDot;
 
@@ -13,20 +17,31 @@ public class FunctionGrapher : MonoBehaviour
     [SerializeField] private float graphMin = -50;
     [SerializeField] private float graphMax = 50;
 
-    private LineRenderer lr;
+    private MeshFilter meshFilter;
+    private MeshFilter backMeshFilter;
+    private GraphChartBase graphChartBase;
 
     private void Awake()
     {
         Instance = this;
-        lr = GetComponent<LineRenderer>();
-        
+        meshFilter = GetComponent<MeshFilter>();
+        backMeshFilter = transform.GetChild(0).GetComponent<MeshFilter>();
+        //graphChartBase = GameObject.Find("GraphMultiple").GetComponent<GraphChartBase>();
+
         GraphFunction(x => 0, Color.red);
         GraphFunctionForY(y => 0, Color.green);
     }
-    
+
     public void GraphFunction(Func<float, float> action, Color color)
     {
         for (var x = graphMin; x < graphMax; x += graphSpacing) SpawnDot(new Vector2(x, action(x)), color);
+    }
+
+    public void GraphFunction2(Func<float, float> action, Color color)
+    {
+        var points = new List<Vector3>();
+        for (var x = graphMin; x < graphMax; x += graphSpacing) points.Add(new Vector3(x, action(x)));
+        DrawCurve(points.ToArray());
     }
 
     public void Graph2DFunction(Func<KeyValuePair<float, float>, KeyValuePair<float, float>> action, Color color)
@@ -36,9 +51,10 @@ public class FunctionGrapher : MonoBehaviour
         for (var x = graphMin; x < graphMax; x += graphSpacing)
         {
             var result = action(new KeyValuePair<float, float>(x, y));
-            if (!float.IsNaN(result.Key) && !float.IsNaN(result.Value)) points.Add(new Vector3(result.Key, result.Value, 0));
+            if (!float.IsNaN(result.Key) && !float.IsNaN(result.Value))
+                points.Add(new Vector3(result.Key, result.Value));
         }
-        
+
         DrawCurve(points.ToArray());
     }
 
@@ -55,7 +71,60 @@ public class FunctionGrapher : MonoBehaviour
 
     private void DrawCurve(Vector3[] points)
     {
-        lr.positionCount = points.Length;
-        lr.SetPositions(points);
+        if (points.Length > 100)
+        {
+            var newPoints = new List<Vector3>();
+            for (int i = 0; i < points.Length; i += points.Length / 100)
+            {
+                newPoints.Add(points[i]);
+            }
+
+            points = newPoints.ToArray();
+        }
+
+        var test = new Vector3[]
+        {
+            new Vector3(0, 0),
+            new Vector3(1, 1),
+            new Vector3(2, 2),
+        };
+        //points = test;
+
+        var thickPoints = new Vector3[points.Length * 2];
+
+        for (var i = 0; i < points.Length; i++)
+        {
+            var heading = i == 0 ? points[i + 1] - points[i] : points[i] - points[i - 1];
+            var direction = heading / heading.magnitude;
+            var perpendicularDirection = new Vector3(direction.y, -direction.x).normalized;
+            thickPoints[i] = points[i] + perpendicularDirection * thickness;
+            thickPoints[thickPoints.Length - i - 1] = points[i] + perpendicularDirection * -thickness;
+        }
+
+        var vertices2D = new Vector2[thickPoints.Length];
+        for (var i = 0; i < thickPoints.Length; i++)
+        {
+            vertices2D[i] = new Vector2(thickPoints[i].x, thickPoints[i].y);
+        }
+
+        var tr = new Triangulator(vertices2D);
+        var indices = tr.Triangulate();
+        var mesh = new Mesh {vertices = thickPoints, triangles = indices};
+        meshFilter.mesh = mesh;
+        var backMesh = new Mesh {vertices = thickPoints, triangles = indices.Reverse().ToArray()};
+        backMeshFilter.mesh = backMesh;
+    }
+
+    public void DrawGraph(List<Vector2> points)
+    {
+        graphChartBase.DataSource.StartBatch();
+        graphChartBase.DataSource.ClearCategory("Player 1");
+        for (var i = 0; i < points.Count; i++)
+        {
+            var point = points[i];
+            graphChartBase.DataSource.AddPointToCategory("Player 1", point.x, point.y);
+        }
+
+        graphChartBase.DataSource.EndBatch();
     }
 }
--- Assets/Scripts/TestClass.cs
+++ Assets/Scripts/TestClass.cs
@@ -18,17 +18,30 @@ public class TestClass : MonoBehaviour
 
     public void Test()
     {
+//        FunctionGrapher.Instance.GraphFunction2(
+//            x => { return slidersW[0].value * Mathf.Pow(x, 2) + slidersW[1].value * x + slidersB[0].value; }, Color.magenta);
+
+        FunctionGrapher.Instance.GraphFunction2(
+            x =>
+            {
+                // x2 = (x1w3 + b2 - x1w1 - b1) / (w2 - w4)
+                return (x * slidersW[2].value + slidersB[1].value - x * slidersW[0].value - slidersB[0].value) / (slidersW[1].value - slidersW[3].value);
+            }, Color.magenta);
+
+        return;
+        
+
         FunctionGrapher.Instance.Graph2DFunction(
             i =>
             {
                 var a1 = SigmoidFunction(i.Key * slidersW[0].value + i.Value * slidersW[1].value + slidersB[0].value);
                 var a2 = SigmoidFunction(i.Key * slidersW[2].value + i.Value * slidersW[3].value + slidersB[1].value);
                 var a3 = SigmoidFunction(i.Key * slidersW[4].value + i.Value * slidersW[5].value + slidersB[2].value);
-                var rp = SigmoidFunction(a1 * slidersW[6].value + a2 * slidersW[7].value + a3 * slidersW[8].value + slidersB[3].value) -
-                         SigmoidFunction(a1 * slidersW[9].value + a2 * slidersW[10].value + a3 * slidersW[11].value + slidersB[4].value);
-                return rp > 0
-                    ? new KeyValuePair<float, float>(float.NaN, float.NaN)
-                    : i;
+                var rp = SigmoidFunction(a1 * slidersW[6].value + a2 * slidersW[7].value + a3 * slidersW[8].value +
+                                         slidersB[3].value) -
+                         SigmoidFunction(a1 * slidersW[9].value + a2 * slidersW[10].value + a3 * slidersW[11].value +
+                                         slidersB[4].value);
+                return Math.Abs(rp) > 0.1f ? new KeyValuePair<float, float>(float.NaN, float.NaN) : i;
             }, Color.magenta);
     }
 
